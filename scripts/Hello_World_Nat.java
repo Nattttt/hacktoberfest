068fb45c301a1c7290a06982f470585505f68334
Nattttt <aylinnataliarv@ufps.edu.co>
2019-10-22 01:02:27 -0500

    New Hello_world

---
+++ scripts/Hello_World_Nat.java
@@ -0,0 +1,11 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Natalia Reyes
+  GITHUB: https://github.com/Nattttt
+*/
+
+public class HelloWJava{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
